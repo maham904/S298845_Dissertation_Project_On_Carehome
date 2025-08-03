30cae408ff375b1ad06647db1fdd6974d2c7aee5
maham904 <mahamkhanlu@gmail.com>
2025-08-04 00:50:48 +0100

    log view issue fixed

--- build.sh
+++ build.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
-# Install dependencies
-poetry install
-
-# Convert poetry.lock to requirements.txt
-poetry export -f requirements.txt --output requirements.txt
-
-# Install Python dependencies
+# Install dependencies from requirements.txt
 pip install -r requirements.txt
 
 # Collect static files
